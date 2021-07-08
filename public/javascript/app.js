ef21f8a46b7b8aa72fe61f5f8fc53887e93ad82b
aaron-watts <retrolasered@gmail.com>
2021-07-08 14:55:46 +0100

    disable current path in nav

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -2,6 +2,6 @@ const navLinks = document.querySelectorAll('.nav-link');
 
 for (let link of navLinks) {
     if (link.pathname === window.location.pathname) {
-        link.classList.add('active');
+        link.classList.add('active', 'disabled');
     }
 }
