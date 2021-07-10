1b4632260a2bba7ad1ea286b9f037f5d36720feb
aaron-watts <retrolasered@gmail.com>
2021-07-10 13:45:03 +0100

    toggle rsvps on events

--- public/javascript/rsvp.js
+++ public/javascript/rsvp.js
@@ -1,36 +1,44 @@
 const rsvpButtons = document.querySelectorAll('button.rsvp.follow');
 const unrsvpButtons = document.querySelectorAll('button.rsvp.unfollow');
 
+const follow = async function () {
+    console.log('follow')
+    const removeButton = function (btn) {
+        const newBtn = document.createElement('BUTTON');
+        newBtn.classList.add('btn', 'text-success', 'btn-sm', 'rsvp', 'unfollow');
+        newBtn.id = btn.id;         
+        const content = document.createElement('I');
+        content.classList.add('bi', 'bi-bookmark-fill');
+        newBtn.appendChild(content);
+        newBtn.addEventListener('click', unfollow)
+        btn.parentElement.appendChild(newBtn)
+        btn.remove();
+    }
+    const res = await axios.put(`/events/${this.id}`)
+        .then(removeButton(this))
+}
+
+const unfollow = async function () {
+    console.log('unfollow');
+    const removeButton = function (btn) {
+        const newBtn = document.createElement('BUTTON');
+        newBtn.classList.add('btn', 'text-secondary', 'btn-sm', 'rsvp', 'follow');
+        newBtn.id = btn.id;     
+        const content = document.createElement('I');
+        content.classList.add('bi', 'bi-bookmark-fill');
+        newBtn.appendChild(content);
+        newBtn.addEventListener('click', follow)
+        btn.parentElement.appendChild(newBtn)
+        btn.remove();
+    }
+    const res = await axios.patch(`/events/${this.id}`)
+        .then(removeButton(this))
+}
+
 for (let button of rsvpButtons) {
-    button.addEventListener('click', async function () {
-        const removeButton = function (btn) {
-            const div = btn.parentElement;
-            const badge = document.createElement('BUTTON');
-            const content = document.createElement('I');
-            content.classList.add('bi', 'bi-bookmark-fill');
-            badge.appendChild(content);
-            badge.classList.add('btn', 'text-success');
-            btn.remove();
-            div.appendChild(badge);
-        }
-        const res = await axios.put(`/events/${this.id}`)
-            .then(removeButton(this))
-    })
+    button.addEventListener('click', follow)
 }
 
 for (let button of unrsvpButtons) {
-    button.addEventListener('click', async function () {
-        const removeButton = function (btn) {
-            const div = btn.parentElement;
-            const badge = document.createElement('BUTTON');
-            const content = document.createElement('I');
-            content.classList.add('bi', 'bi-bookmark-fill');
-            badge.appendChild(content);
-            badge.classList.add('btn', 'text-success');
-            btn.remove();
-            div.appendChild(badge);
-        }
-        const res = await axios.patch(`/events/${this.id}`)
-            //.then(removeButton(this))
-    })
+    button.addEventListener('click', unfollow)
 }
