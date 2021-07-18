02c069bd72b22850cd9f4b392c8c0f81f147dc61
aaron-watts <retrolasered@gmail.com>
2021-07-18 17:43:35 +0100

    add about page

--- app.js
+++ app.js
@@ -88,6 +88,10 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 })
