d7a06647aec60bd694b0eab04876e9137350c60c
aaron-watts <retrolasered@gmail.com>
2021-08-07 15:26:42 +0100

    add modals for spot input in event forms as mobile does not support bootstrap datasets

--- public/javascript/eventForm.js
+++ public/javascript/eventForm.js
@@ -1,5 +1,8 @@
 const form = document.querySelector('form#events-form');
 const datetime = ['day', 'month', 'year', 'hours', 'minutes'];
+const modal = document.querySelector('#spot-select');
+const list = document.querySelector('#spot-select ul');
+const listInput = document.querySelector('#list-input');
 
 const checkNotBlankGlobal = (input) => {
     if (input.value.length) {
@@ -16,25 +19,41 @@ const numberInputMaxLength = (target) => {
     else target.value = target.value.slice(0, 2);
 }
 
+// check if the spot is a valid spot that exists within the database
 const spotValid = (spot) => {
     const feedback = document.querySelector('#spot-feedback');
+
     if (spot.value && spots.some(i => i === spot.value)) {
-        spot.classList.remove('is-invalid'); 
-        spot.classList.add('is-valid'); 
+
+        spot.classList.remove('is-invalid');
+        spot.classList.add('is-valid');
+
+        // hide tooltip
         feedback.classList.add('d-none');
+
+        // margin fix
         spot.parentElement.classList.add('mb-3');
         spot.parentElement.classList.remove('mb-5');
+
         return true;
+
     } else {
-        spot.classList.add('is-invalid'); 
-        spot.classList.remove('is-valid');  
+
+        spot.classList.add('is-invalid');
+        spot.classList.remove('is-valid');
+
+        // show tooltip
         feedback.classList.remove('d-none');
+
+        // margin fix
         spot.parentElement.classList.remove('mb-3');
         spot.parentElement.classList.add('mb-5');
+
         return false;
     }
 }
 
+// a function to assist with date and time inputs to help prevent user error
 const dateHandler = () => {
     const mm = month.value || '0';
 
@@ -88,8 +107,8 @@ const dateHandler = () => {
 
     // dates must be upcoming
     backToTheFuture(
-        parseInt(day.value), 
-        parseInt(month.value) - 1, 
+        parseInt(day.value),
+        parseInt(month.value) - 1,
         parseInt(year.value),
         parseInt(hours.value),
         parseInt(minutes.value)
@@ -105,6 +124,71 @@ const dateHandler = () => {
         blinkInput(minutes);
     }
 }
+
+const filterSpots = () => {
+    // delete existing spots
+    for (let item = list.children.length - 1; item >= 0; item--) {
+        list.children[item].remove();
+    }
+
+    // if search term exists, show filtered list
+    if (listInput.value.length) {
+        const filteredSpots = spots.filter(spot => spot.toLowerCase().includes(listInput.value.toLowerCase()));
+
+        if (filteredSpots.length) {
+            filteredSpots.forEach(spot => {
+                const li = document.createElement('li');
+                li.classList.add('list-group-item');
+                li.innerText = spot;
+                li.addEventListener('click', populateSpotInput);
+                list.appendChild(li);
+            })
+        }
+    }
+
+    // if search term absent, show full list
+    if (!listInput.value.length) {
+        spots.forEach(spot => {
+            const li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.innerText = spot;
+            li.addEventListener('click', populateSpotInput);
+            list.appendChild(li);
+        })
+    }
+}
+
+// disable form elements while modal is open
+const disableNonModalInputs = (disable=true) => {
+    for (input of form) {
+        input.disabled = disable;
+    }
+}
+
+// take selected spot name and insert into spot input field
+const populateSpotInput = function () {
+    form.spot.value = this.innerText;
+    listInput.value = '';
+    filterSpots();
+    disableNonModalInputs(false);
+    modal.classList.add('d-none');
+}
+
+// add evetn listeners to list items
+for (let listItem of list.children) {
+    listItem.addEventListener('click', populateSpotInput)
+}
+
+// show modal for spot list
+form.spot.addEventListener('click', function () {
+    this.blur();
+    disableNonModalInputs();
+    modal.classList.remove('d-none');
+    listInput.focus();
+})
+
+// filter spot list on typing
+modal.addEventListener('input', filterSpots)
 
 form.formSubmit.addEventListener('click', () => {
     const validateForm = () => {
@@ -138,9 +222,9 @@ form.formSubmit.addEventListener('click', () => {
         checkNotBlankLocal(description);
 
         return validated;
-    }  
+    }
 
-    if(validateForm()) form.submit();
+    if (validateForm()) form.submit();
 })
 
 form.addEventListener('change', (evt) => {
